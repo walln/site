004e3b0c1d0b6d5761ce18394a02b46b85fcc39a
#46641379 <nickwall2721@gmail.com>
2021-02-01 09:12:20 -0600

    updated css-purging config for prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: 'class',
   theme: {
     extend: {
