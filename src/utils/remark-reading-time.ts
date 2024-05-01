4776533b50a677ab38600d9204fb4c84d7584b65
Nick Wall <46641379+walln@users.noreply.github.com>
2024-04-30 19:03:24 -0500

    feat(site): pre-commit configs

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,8 +1,18 @@
 import getReadingTime from "reading-time";
 import { toString as contentToString } from "mdast-util-to-string";
 
+interface Data {
+	data: {
+		astro: {
+			frontmatter: {
+				minutesRead: string;
+			};
+		};
+	};
+}
+
 export function remarkReadingTime() {
-	return (tree: unknown, { data }: any) => {
+	return (tree: unknown, { data }: Data) => {
 		const textOnPage = contentToString(tree);
 		const readingTime = getReadingTime(textOnPage);
 		data.astro.frontmatter.minutesRead = readingTime.text;
